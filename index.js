704e11ac7337e8dff35002f432bf19136b671bca
Ehad <ehadberisha@gmail.com>
2023-08-31 13:01:47 -0400

    finished

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if (someValue < 400) result = `This one is on me!`;
+  if (someValue > 400 && someValue < 2000)
+    result = `That will be twenty bucks.`;
+  if (someValue > 2000 && someValue < 2500)
+    result = "I will gladly take your thirty bucks.";
+  if (someValue > 2500) result = "No can do.";
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result;
+  if (city == "NYC") {
+    result = "Ok, sounds good.";
+  } else {
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
